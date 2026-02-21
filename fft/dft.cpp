a8d1039c8658a5deb0adf042d08f2747cf055e42
Thomas <59429963+tfhammond@users.noreply.github.com>
2026-02-21 14:49:10 -0800

    cooley-tukey

--- fft/dft.cpp
+++ fft/dft.cpp
@@ -3,33 +3,53 @@
 #include <cmath>
 #include <numbers>
 #include <iostream>
+#include <string>
 
 using cd = std::complex<double>;
 constexpr double PI = std::numbers::pi;
 
 std::vector<cd> dft(const std::vector<cd>& x) {
-
 	int n = static_cast<int>(x.size());
-
 	std::vector<cd> ft(n, cd(0.0, 0.0));
-
-
-
 	for (int k = 0; k < n; k++) {
-
 		cd sum{0.0, 0.0};
-
 		for (int m = 0; m < n; m++) {
 			const cd angle = cd(0.0, -2.0 * PI * k * m / n);
 			sum += x[m] * std::exp(angle);
 		}
-	
 		ft[k] = sum;
-	
 	}
-
 	return ft;
 }
+
+void fft_rec(cd* input, cd* output, int N, int stride) {
+
+    if (N == 1) {
+        output[0] = input[0];
+    }
+    else {
+        fft_rec(input, output, N / 2, 2 * stride);
+        fft_rec(input + stride, output + N / 2, N / 2, 2 * stride);
+        for (int k = 0; k < (N / 2); k++) {
+            // need to implement.
+            cd p = output[k];
+            cd angle = cd(0.0, -2.0 * PI * k / N);
+            cd q = std::exp(angle) * output[k + N / 2];
+            output[k] = p + q;
+            output[k + N / 2] = p - q;
+        }
+    }
+
+}
+
+// n must be a power of 2
+std::vector<cd> ctfft(const std::vector<cd>& input){
+    std::vector<cd> input_ = input;
+    std::vector<cd> output(input.size());
+    fft_rec(input_.data(), output.data(), input_.size(), 1);
+    return output;
+}
+
 
 
 int main() {
